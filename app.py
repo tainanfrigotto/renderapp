dc564b3793c2b2f7d254f0996abc6b64d3f64fa1
Tainan Frigotto de Lima <141517700+tainanfrigotto@users.noreply.github.com>
2025-09-07 16:30:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,2 @@
-import streamlit as st
-
-st.header('Jogando uma moeda')
-
-st.write('Ainda não é um aplicativo funcional. Em construção.')
+ls -a
+.git .gitignore .streamlit app.py README.md requirements.txt
