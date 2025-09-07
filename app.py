5152540985ecd703a6f929f6691572a24450878a
Tainan Frigotto de Lima <141517700+tainanfrigotto@users.noreply.github.com>
2025-09-07 16:55:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,5 @@
-ls -a
-.git .gitignore .streamlit app.py README.md requirements.txt
+import streamlit as st
+
+st.header('Jogando uma moeda')
+
+st.write('Ainda não é um aplicativo funcional. Em construção.')
